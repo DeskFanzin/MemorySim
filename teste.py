7781276c583dc9e54cd1b4e9446145702f4d0c65
Gabriel Martins <gabriel.jardel.matheus@gmail.com>
2022-09-13 18:02:39 -0300

    tentando MRU

--- teste.py
+++ teste.py
@@ -2,8 +2,9 @@
 def MRU():
     contagemTrocasMRU = 0
     ##sequenciaPags = [10, 23, 15, 1, 27, 25, 5, 12, 10, 4, 20, 4, 13, 22, 24, 2, 17, 16, 8, 15, 9, 23, 9, 9, 14, 14, 16, 26, 16, 2, 10, 21, 22, 14, 7, 11, 4, 26, 14, 24, 16, 5, 6, 23, 15, 15, 3, 9, 23, 15, 26, 23, 3, 4, 22, 5, 4, 10, 14, 19, 24, 26, 8, 17, 21, 17, 10, 13, 21, 11, 17, 6, 9, 27, 2, 8, 17, 4, 14, 12, 2]
-    sequenciaPags = [1, 2, 3, 4, 5, 6, 3, 4, 4, 5, 4, 3, 3, 4, 5, 6, 3, 4, 1, 7, 8]
-    tamanhoMoldura = 3
+    ##sequenciaPags = [1, 2, 3, 4, 5, 6, 3, 4, 4, 5, 4, 3, 3, 4, 5, 6, 3, 4, 1, 7, 8]
+    sequenciaPags = [1, 2, 7, 2, 3, 2, 2, 3, 4, 2, 1, 3, 1, 4, 5, 5, 6, 1, 3, 2, 6, 7, 7, 7, 8]
+    tamanhoMoldura = 4
     quantPags = 8
     moldura = []
     tempodeUso = {}
@@ -15,29 +16,32 @@ def MRU():
         if tamanhoMoldura == len(moldura):
             if sequenciaPags[j] in moldura:
                 for k in moldura:
-                    if k != sequenciaPags[j]:
-                        tempodeUso[k] += 1
+                    if k == sequenciaPags[j]:
+                        tempodeUso[k] = tempodeUso[k]
+                    else:
+                        tempodeUso[k] -= 1                
             else:
                 ##verifica qual página tem o menor tempo de uso e a substitui
                 tempodeUsoMoldura = {}
                 for k in sorted(moldura):
                     tempodeUsoMoldura[k] = tempodeUso[k]
                 menor = min(tempodeUsoMoldura, key=tempodeUsoMoldura.get)
-                print(tempodeUsoMoldura)
                 moldura.pop(moldura.index(menor))
-                moldura.append(sequenciaPags[j])
                 for k in moldura:
-                    if k != sequenciaPags[j]:
-                        tempodeUso[k] += 1
+                    tempodeUso[k] -= 1
+                moldura.append(sequenciaPags[j])
                 contagemTrocasMRU += 1
         else:
             if sequenciaPags[j] in moldura:
                 for k in moldura:
-                    if k != sequenciaPags[j]:
-                        tempodeUso[k] += 1
+                    if k == sequenciaPags[j]:
+                        tempodeUso[k] = tempodeUso[k]
+                    else:
+                        tempodeUso[k] -= 1
             else:
+                ##diminui 1 de tempo de uso de cada página da moldura
                 for k in moldura:
-                    tempodeUso[k] += 1
+                    tempodeUso[k] -= 1
                 moldura.append(sequenciaPags[j])
                 contagemTrocasMRU += 1
     moldura.clear()
